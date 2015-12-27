a344db39b1a777b1cca8ba8b0a3d751550531e60
Alan Wong <pyepye@gmail.com>
2015-12-27 09:55:18 +0000

    Set root to pages#home

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/home', to: 'pages#home'
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
