845d87864aa0b87d9771538a122621a8dc21389b
Alan Wong <pyepye@gmail.com>
2016-01-06 09:39:30 +0000

    Add validation and tests to receipe model

---
+++ test/models/recipe_test.rb
@@ -0,0 +1,59 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+
+  def setup
+    @recipe = Recipe.new(name: "Chicken Parm", summary: "Best Chicken Parm ever",
+              description: "heat oil, add onions, add tomato sauce, add chicken, cook for 20 minutes")
+  end
+
+  test "recipe should be valid" do
+    assert @recipe.valid?
+  end
+
+  test "name should be present" do
+    @recipe.name = ""
+    assert_not @recipe.valid?
+  end
+
+  test "name length should not be too long" do
+    @recipe.name = "n" * 101
+    assert_not @recipe.valid?
+  end
+
+  test "name length should not be too short" do
+    @recipe.name = "nnnn"
+    assert_not @recipe.valid?
+  end
+
+  test "summary should be present" do
+    @recipe.summary = ""
+    assert_not @recipe.valid?
+  end
+
+  test "summary length should not be too long" do
+    @recipe.summary = "s" * 151
+    assert_not @recipe.valid?
+  end
+
+  test "summary length should not be too short" do
+    @recipe.summary = "s" * 9
+    assert_not @recipe.valid?
+  end
+
+  test "description should be present" do
+    @recipe.description = ""
+    assert_not @recipe.valid?
+  end
+
+  test "description length should not be too long" do
+    @recipe.description = "d" * 501
+    assert_not @recipe.valid?
+  end
+
+  test "description length should not be too short" do
+    @recipe.description = "d" * 19
+    assert_not @recipe.valid?
+  end
+
+end
