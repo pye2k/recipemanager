91f507a8f358a7dfe1b6cd27df0c75eb789a1f73
Alan Wong <pyepye@gmail.com>
2016-01-16 07:43:49 +0000

    Add validations for likes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -42,9 +42,15 @@ class RecipesController < ApplicationController
 
   def like
     @recipe = Recipe.find(params[:id])
-    Like.create(like: params[:like], chef: @recipe.chef, recipe: @recipe)
-    flash[:success] = "Your selection was successfully logged"
-    redirect_to :back
+    like = Like.create(like: params[:like], chef: @recipe.chef, recipe: @recipe)
+    if like.valid?
+      flash[:success] = "Your selection was successfully logged"
+      redirect_to :back
+    else
+      flash[:danger] = "You can only like/dislike a recipe once"
+      redirect_to :back
+    end
+
   end
 
   private
