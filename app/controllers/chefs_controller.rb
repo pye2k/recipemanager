9050f3783d648762ad38021c4de219dc470f82aa
Alan Wong <pyepye@gmail.com>
2016-01-17 00:10:39 +0000

    Add show chef action

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -27,7 +27,11 @@ class ChefsController < ApplicationController
     else
       render 'edit'
     end
+  end
 
+  def show
+    @chef = Chef.find(params[:id])
+    @recipes = @chef.recipes.paginate(page: params[:page], per_page: 3)
   end
 
   private
