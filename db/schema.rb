e92c7c1a6bf59c908c9fb964135e814b12f912c2
Alan Wong <pyepye@gmail.com>
2016-01-19 07:35:37 +0000

    Add styles table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160116091607) do
+ActiveRecord::Schema.define(version: 20160119073424) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -37,6 +37,10 @@ ActiveRecord::Schema.define(version: 20160116091607) do
     t.datetime "updated_at"
     t.integer  "chef_id"
     t.string   "picture"
+  end
+
+  create_table "styles", force: :cascade do |t|
+    t.string "name"
   end
 
 end
