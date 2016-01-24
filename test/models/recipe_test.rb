e1e6211151c862a51c66f54fec616b5143ab1079
Alan Wong <pyepye@gmail.com>
2016-01-24 20:11:05 +0000

    Fix tests by giving it a valid chef

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
 
   def setup
-    @chef = Chef.create(chefname: "Bob", email: "bob@example.com")
+    @chef = Chef.create(chefname: "Bob", email: "bob@example.com", password: "password")
     @recipe = @chef.recipes.build(name: "Chicken Parm", summary: "Best Chicken Parm ever",
               description: "heat oil, add onions, add tomato sauce, add chicken, cook for 20 minutes")
   end
