808ecba0eee62b558c097c65c4aa332bbe5b3a0e
Alan Wong <pyepye@gmail.com>
2016-01-26 08:13:59 +0000

    Add associations between reviews and chefs, and reviews and recipes

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ActiveRecord::Base
+  belongs_to :recipe
+  belongs_to :chef
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true, length: { minimum: 20 }
 end
