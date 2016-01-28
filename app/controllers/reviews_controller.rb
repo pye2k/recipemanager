c4ea490beef29ab9d75a5d317f21105728594855
Alan Wong <pyepye@gmail.com>
2016-01-28 07:57:24 +0000

    Ensure that chefs cannot review their own recipes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,20 @@
 class ReviewsController < ApplicationController
+  before_action :set_recipe
   before_action :require_user, except: [:show]
+  before_action :require_different_user, except: [:show]
 
   def new
     @review = Review.new
-    @recipe = Recipe.find(params[:recipe_id])
   end
 
   def create
     @review = Review.new(review_params)
     @review.chef = current_user
-    @review.recipe_id = params[:recipe_id]
+    @review.recipe = @recipe
 
     if @review.save
       flash[:success] = "Your review was created successfully!"
-      redirect_to recipe_review_path(params[:recipe_id], @review)
+      redirect_to recipe_review_path(@recipe, @review)
     else
       render :new
     end
@@ -27,6 +28,17 @@ class ReviewsController < ApplicationController
 
     def review_params
       params.require(:review).permit(:title, :body)
+    end
+
+    def set_recipe
+      @recipe = Recipe.find(params[:recipe_id])
+    end
+
+    def require_different_user
+      if current_user == @recipe.chef
+        flash[:danger] = "You cannot review your own recipes"
+        redirect_to recipe_path(@recipe)
+      end
     end
 
 end
